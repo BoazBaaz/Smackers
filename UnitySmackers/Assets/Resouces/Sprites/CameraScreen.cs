9bf05aed293a1fd1dee1064f46018e57b10ec1ad
BoazBaaz <Boas.Bas@gmail.com>
2020-02-17 09:12:20 +0100

    17/02/2020 09:10

---
+++ UnitySmackers/Assets/Resouces/Sprites/CameraScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScreen : MonoBehaviour
+{
+    public GameObject m_Player1;
+    public GameObject m_Player2;
+
+    void Start()
+    {
+        m_Player1 = GameObject.FindGameObjectWithTag("Player1").GetComponent<GameObject>();
+        m_Player2 = GameObject.FindGameObjectWithTag("Player2").GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

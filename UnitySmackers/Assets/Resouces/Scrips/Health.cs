0f7564ceef9b0e2557a63c2e7c60b75835988ff6
BoazBaaz <Boas.Bas@gmail.com>
2020-03-24 11:39:22 +0100

    24/03/2020 11:39
    First commit

---
+++ UnitySmackers/Assets/Resouces/Scrips/Health.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public Canvas canvas;
+    public Text text;
+
+    public Rigidbody playerRigidbody;
+    public Rigidbody targetRigidbody;
+
+    public LayerMask layerMask;
+
+    public float raycastRange = 10f;
+
+    public float HitForce;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public int damage = 10;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    private void Update()
+    {
+        if (RaycastEnemy())
+            Hit();
+
+        if (currentHealth > 0 && Input.GetKeyDown(KeyCode.Space))
+            EndMatch();
+    }
+
+    private bool RaycastEnemy()
+    {
+        Vector3 directionToTarget = (targetRigidbody.position - playerRigidbody.position).normalized;
+
+        bool isHit = Physics.Raycast(playerRigidbody.position, directionToTarget, raycastRange, layerMask);
+
+        Debug.DrawRay(playerRigidbody.position, directionToTarget * raycastRange, Color.red);
+
+        if (isHit)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void Hit()
+    {
+        targetRigidbody.SendMessage("GetHit", damage);
+    }
+
+    private void GetHit(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    private void EndMatch()
+    {
+        Time.timeScale = 0f;
+
+        text.text = targetRigidbody.name + " won.";
+        canvas.enabled = true;
+    }
+}
---
+++ UnitySmackers/Assets/Resouces/Scrips/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public KeyCode left;
+    //public KeyCode right;
+    //public KeyCode forward;
+    //public KeyCode Backward;
+
+    public Rigidbody rigidbodyPlayer;
+    public float moveSpeed = 20;
+
+    private Vector3 vectorInput;
+
+    private void FixedUpdate()
+    {
+        if (rigidbodyPlayer.CompareTag("Player2"))
+        {
+            vectorInput.x = Input.GetAxis("Horizontal2");
+            vectorInput.z = Input.GetAxis("Vertical2");
+
+            Vector3 newPos = rigidbodyPlayer.position + ((vectorInput * moveSpeed) * Time.fixedDeltaTime);
+
+            rigidbodyPlayer.MovePosition(newPos);
+        }
+        if (rigidbodyPlayer.CompareTag("Player1"))
+        {
+            vectorInput.x = Input.GetAxis("Horizontal1");
+            vectorInput.z = Input.GetAxis("Vertical1");
+
+            Vector3 newPos = rigidbodyPlayer.position + ((vectorInput * moveSpeed) * Time.fixedDeltaTime);
+
+            rigidbodyPlayer.MovePosition(newPos);
+        }
+    }
+
+    private void Update()
+    {
+        #region Old Code
+        //if (Input.GetKey(left))
+        //{
+        //    transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        //}
+        //if (Input.GetKey(right))
+        //{
+        //    transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+
+        //}
+        //if (Input.GetKey(forward))
+        //{
+        //    transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        //}
+        //else if (Input.GetKey(Backward))
+        //{
+        //    transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        //}
+        #endregion
+    }
+}

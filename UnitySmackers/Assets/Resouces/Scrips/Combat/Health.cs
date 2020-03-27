f35e9cd5193eadc3b89d9a30fac5e482a60a7e79
BoazBaaz <Boas.Bas@gmail.com>
2020-03-27 11:20:22 +0100

    27/03/2020 11:20

---
+++ UnitySmackers/Assets/Resouces/Scrips/Combat/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float m_MaxHealth = 100f;
+    public float m_CurrentHealth;
+
+    public float m_Damage = 10f;
+
+    private void Start()
+    {
+        m_CurrentHealth = m_MaxHealth;
+    }
+
+    //Maby Headshots more damage later???
+
+    public void GetHit()
+    {
+        m_CurrentHealth -= m_Damage;
+    }
+}
